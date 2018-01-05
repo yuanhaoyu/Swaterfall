24a12a050afd83430c7b84ddbdeb1fa2d6f739f9
yuanhaoyu <yhy929768691@gmail.com>
2018-01-05 16:41:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,35 @@
+# Swaterfall
+---
+Swaterfall 是一款简单麻瓜的图片瀑布流插件,用固定列的方式实现瀑布流。(注意当前版本只适用于带图片的瀑布流)
+
+## How to use
+1.	初始化
+
+	```javascript
+	      var swaterfall = new Swaterfall(
+	        data,
+	        {
+	            boxs: listDoms,
+	            templates: templates
+	        }
+	    )
+	```
+	其中构造函数，接受2个参数 data 是json数组，也就是你瀑布流里面要加载的数据，第二个参数是个对象，里面有boxs和templates 2个属性，boxs是你要布局的列的dom对象数组，templates是你要生成的dom元素数组。
+
+2. init
+
+	```javascript
+	swaterfall.init()
+	```
+	生成瀑布流
+
+3. add
+
+	```javascript
+	swaterfall.add(data, templates)
+	```
+	添加内容，data和templates如初始化的介绍。
+
+## Feature
+
+- [ ] 将模版dom 升级为模版方法
---
+++ index.js
@@ -0,0 +1,43 @@
+class Swaterfall{
+    constructor (data, config) {
+        this._index = 0;
+        this.data = data;
+        this.boxLists = config.boxs;
+        this.templates = config.templates;
+    }
+    _combine() {
+        if (this._index >= data.length) {
+            return false;
+        } else {
+            let img = new Image();
+            img.src = this.data[this._index].src;
+            img.onload = () => {
+                setTimeout(() => {
+                    let minDom = this._getMinFromDoms(this.boxLists);
+                    console.log(this.templates[this._index])
+                    minDom.appendChild(this.templates[this._index]);
+                    this._index ++;
+                    return this._combine();
+                }, 0)
+            }
+        }
+    }
+    _getMinFromDoms(doms) {
+        let min = doms[0];
+        for (let i = 1; i < doms.length; i++) {
+            if (doms[i].clientHeight < doms[i - 1].clientHeight) {
+                min = doms[i];
+            }
+        }
+        return min;
+    }
+    init() {
+        this._combine();
+    }
+    add(data, templates) {
+        this.templates = templates;        
+        this.data = data;
+        this._index = 0;
+        this._combine();
+    }
+}
